d8eb16b02d4a6fc128a3e4b0b017650a4deb8d23
Bonsuck Koo <bonsuckkoo@gmail.com>
2024-10-17 22:01:59 -0500

    2:00 mark

--- Youtube.cpp
+++ Youtube.cpp
@@ -1,28 +1,122 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+
 using namespace std;
+bool playGame(int guesses)
+{
+    cout << "Playing Game...\n";
+    int correct;
+    srand(time(NULL));
+    if (guesses == 1)
+    {
+        correct = rand() % 200;
+    }
+    else
+    {
+        correct = rand() % 20;
+    }
+    cout << correct << endl;
+    int guessCount = 0;
+    cout << "You get " << guesses << " guesses. \n";
+    while (guessCount < guesses)
+    {
+        cout << "Guess a number...";
 
-int main(){
+        int guess;
+        cin >> guess;
+        if (guess == correct)
+        {
+            return true;
+        }
+        else if (guess < correct)
+        {
+            cout << "Your guess is low\n";
+        }
+        else
+        {
+            cout << "Guess is too high\n";
+        }
+        guessCount++;
+        cout << "You've guessed " << guessCount << " times. \n";
+    }
+    return false;
+}
 
+int main()
+{
+    // cout << true; // boolean literal
+    // cout << "true"; //string literal
     cout << "Do you want to play a game? (y/n)";
-    char response = tolower(getchar()); //Y
+    char response = tolower(getchar()); // Y
     // char responseFiltered = tolower(response); //y
     // int response;
     // cin >> response;
-    // char r = static_cast<char>(response); //Convert int 'response' to char to display as a char 
+    // char r = static_cast<char>(response); //Convert int 'response' to char to display as a char
     // cout << "You entered: " << response << endl;
     // cout << "You entered: " << r << endl;
     cout << "You entered: " << char(response) << endl;
     // if(tolower(response) == 'y') //yes
-     if(response == 'y') //yes
+    if (response == 'y') // yes
     {
-       cout << "Let's play a game, then...\n" ;
+        cout << "Let's play a game, then...\n";
+        cout << "Have you beat the game? \n0: no. \n1:yes.\n";
+        bool beatGame;
+        cin >> beatGame;
+        bool won;
+        if (beatGame)
+        {
+            cout << "Because you beat the game you can play impossible mode.\n";
+            won = playGame(1);
+        }
+        else
+        {
+            cout << "What Difficulty?:";
+            cout << " \n0: easy";
+            cout << " \n1: medium";
+            cout << "\n2:hard\n ";
+            int difficulty;
+            cin >> difficulty;
+
+            switch (difficulty)
+            {
+            case 0: // easy
+                won = playGame(10);
+                break;
+            case 1: // medium
+                won = playGame(5);
+                break;
+            case 2: // hard
+                won = playGame(3);
+                break;
+            default:
+                cout << "Don't understand";
+                break;
+            }
+            //    if(difficulty == "easy" || difficulty == "medium" || difficulty == "hard") {
+            //         cout << "Respawning";
+            //    }
+            //    else if(difficulty == "impossible" && beatGame){
+            //     cout << "LOL good luck\n";
+            //    } else if (difficulty == "impossible" && !beatGame){
+            //     cout << "You have to beat the game at least once.";
+            //    }
+        }
+        if (won)
+        {
+            cout << "Congratz";
+        }
+        else
+        {
+            cout << "You lost, Try again";
+        }
     }
     else if (response == 'n')
     {
         cout << "Maybe next time. Goodbye .\n";
     }
-    else{
+    else
+    {
         cout << "I don't understand. \n";
     }
     // system("pause");
